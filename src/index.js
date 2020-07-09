8ca6ce134fbc8e27352be77cb841bc02cce5300b
Amit <29959138+maverickamit@users.noreply.github.com>
2020-07-10 00:36:13 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import JavascriptTimeAgo from 'javascript-time-ago';
+import { HashRouter as Router } from 'react-router-dom';
 import en from 'javascript-time-ago/locale/en';
 import './index.css';
 import App from './App';
@@ -14,7 +15,12 @@ JavascriptTimeAgo.locale(en);
 
 // console.log = function () {};
 
-ReactDOM.render(<App feedsStore={feedsStore} />, document.getElementById('root'));
+ReactDOM.render(
+	<Router basename="/">
+		<App feedsStore={feedsStore} />
+	</Router>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
