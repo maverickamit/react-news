ec94125dbd21de46b6359db4e5fabaf3568a6d9c
Amit <29959138+maverickamit@users.noreply.github.com>
2020-07-10 00:28:55 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import SettingsPage from "./SettingsPage";
-import "./App.css";
-import TopBar from "./TopBar";
-import TopFeeds from "./TopFeeds";
-import { observer } from "mobx-react";
+import React from 'react';
+
+import { Router, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+
+import { createBrowserHistory as createHistory } from 'history';
+import './App.css';
+import TopBar from './TopBar';
+import FeedPage from './FeedPage';
+import TopFeeds from './TopFeeds';
+
+import SettingsPage from './SettingsPage';
+
+import { observer } from 'mobx-react';
+const history = createHistory();
 
 function App({ feedsStore }) {
-  return (
-    <div
-      className={
-        feedsStore.darkmode != true ? "app bg-white" : "app bg-dark text-light"
-      }
-    >
-      <TopBar feedsStore={feedsStore} />
-      <Route
-        path="/"
-        exact
-        component={(props) => <TopFeeds {...props} feedsStore={feedsStore} />}
-      />
-      <Route
-        path="/settings"
-        exact
-        component={(props) => (
-          <SettingsPage {...props} feedsStore={feedsStore} />
-        )}
-      />
-    </div>
-  );
+	return (
+		<div className={feedsStore.darkmode != true ? 'app bg-white' : 'app bg-dark text-light'}>
+			<Router history={history}>
+				<TopBar feedsStore={feedsStore} />
+				<Route path="/" exact component={(props) => <TopFeeds {...props} feedsStore={feedsStore} />} />
+				<Route
+					path="/settings"
+					exact
+					component={(props) => <SettingsPage {...props} feedsStore={feedsStore} />}
+				/>
+			</Router>
+		</div>
+	);
 }
 export default observer(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import JavascriptTimeAgo from 'javascript-time-ago';
-import { HashRouter as Router } from 'react-router-dom';
 import en from 'javascript-time-ago/locale/en';
 import './index.css';
 import App from './App';
@@ -15,12 +14,7 @@ JavascriptTimeAgo.locale(en);
 
 // console.log = function () {};
 
-ReactDOM.render(
-	<Router basename="/">
-		<App feedsStore={feedsStore} />
-	</Router>,
-	document.getElementById('root')
-);
+ReactDOM.render(<App feedsStore={feedsStore} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
