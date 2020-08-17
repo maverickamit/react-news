880aa6bb32fb72f4829c7365433fbedfbab5ce66
Amit <29959138+maverickamit@users.noreply.github.com>
2020-08-18 00:50:04 +0530

    Updates

---
+++ precache-manifest.4820d4354f11424f8e787837f111ed70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4485645c6ac64be67e2194aeae3767dd",
+    "url": "/amit-ghosh.com/react-news/index.html"
+  },
+  {
+    "revision": "7f667762020ab8c6f74f",
+    "url": "/amit-ghosh.com/react-news/static/css/main.06576a2c.chunk.css"
+  },
+  {
+    "revision": "8c9583e72a24c2675436",
+    "url": "/amit-ghosh.com/react-news/static/js/2.f158ec9b.chunk.js"
+  },
+  {
+    "revision": "6a6d018de47dd770fc6fb6b2eb062408",
+    "url": "/amit-ghosh.com/react-news/static/js/2.f158ec9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f667762020ab8c6f74f",
+    "url": "/amit-ghosh.com/react-news/static/js/main.b976ae06.chunk.js"
+  },
+  {
+    "revision": "3e748f9b3d6719965ac4",
+    "url": "/amit-ghosh.com/react-news/static/js/runtime-main.a6cc1569.js"
+  }
+]);
